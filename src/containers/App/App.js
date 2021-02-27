e9b9e63f2573c23b97135489baec1dd953f62eb6
Stefano Borzoni <isama92@gmail.com>
2021-02-27 15:26:38 +0100

    app + index

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,23 +1,35 @@
 import React from 'react';
+import { Container, Col, Row } from 'react-bootstrap';
+import WeatherCity from 'components/WeatherCity/WeatherCity';
+import classes from './App.module.css';
 
-function App() {
+const app = () => {
+    const data = {
+        city: 'Turin',
+        date: 'Friday 18, September',
+        weatherName: 'Sunny',
+        weatherDegrees: 22,
+        weatherIcon: '01d',
+    };
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="https://reactjs.org"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <Container fluid className={classes.Container}>
+            <Row>
+                <Col xs={12} md={8}>
+                    <WeatherCity
+                      city={data.city}
+                      date={data.date}
+                      weatherName={data.weatherName}
+                      weatherDegrees={data.weatherDegrees}
+                      weatherIcon={data.weatherIcon}
+                    />
+                </Col>
+                <Col xs={12} md={2}>
+                    side
+                </Col>
+            </Row>
+        </Container>
     );
-}
+};
 
-export default App;
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
 import App from 'containers/App/App';
 import reportWebVitals from './reportWebVitals';
+import store from './redux-store';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root'),
 );
